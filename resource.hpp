be40ceae17f817d7b66e67079e6c06efc736109e
Neeraj <neeraj.kumar@acronis.com>
2017-02-07 18:55:06 +0800

    resource class

---
+++ resource.hpp
@@ -0,0 +1,15 @@
+
+
+
+class Resource {
+
+public:
+  //Resource(std::string path, /* function ?? */);  
+
+private:
+  Resource(const Resource& other) = delete;
+  Resource& operator=(const Resource& rhs) = delete;
+
+  //std::map<std::string, std::function ??
+};
+
